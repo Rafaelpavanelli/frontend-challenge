62f0d553d1c6c94dd1f8a374a89f768a8e466427
Rafael Pavanelli <rafaelzica86@hotmail.com>
2024-04-10 10:29:34 -0300

    .

--- frontend-challenge/src/app/Components/HerosList/index.tsx
+++ frontend-challenge/src/app/Components/HerosList/index.tsx
@@ -13,39 +13,38 @@ export const HeroList = () => {
   );
   const [page, setPage] = useState(0);
   useEffect(() => {
-    dispatch(fetchCharactersData({ limit: 18,offset: page * 18 }))
-  }, [dispatch,page]);
+    dispatch(fetchCharactersData({ limit: 18, offset: page * 18 }));
+  }, [dispatch, page]);
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
-  if(charactersData.data.result) return(
-  <>
-   <Typography
-        variant="h1"
-        zIndex={1}
-        color={"white"}
-        textAlign={"center"}
-        fontFamily={"Roboto"}
-        margin={"40px 0px"}
-      >
-        All heroes
-      </Typography>
+  if (charactersData.data.result)
+    return (
+      <>
+        <Typography
+          variant="h1"
+          zIndex={1}
+          color={"white"}
+          textAlign={"center"}
+          fontFamily={"Roboto"}
+          margin={"40px 0px"}
+        >
+          All heroes
+        </Typography>
         <Box
           display={"flex"}
           flexWrap={"wrap"}
           justifyContent={"center"}
           gap={10}
-          sx={{
-          
-          }}
+          sx={{}}
         >
           {Array.from({ length: 18 }, (_, i) => (
             <CardLoading key={i} />
           ))}
         </Box>
-        </>
-  )
+      </>
+    );
   return (
     <>
       <Typography
@@ -64,9 +63,7 @@ export const HeroList = () => {
           flexWrap={"wrap"}
           justifyContent={"center"}
           gap={10}
-          sx={{
-          
-          }}
+          sx={{}}
         >
           {Array.from({ length: 18 }, (_, i) => (
             <CardLoading key={i} />
@@ -80,34 +77,40 @@ export const HeroList = () => {
           flexWrap={"wrap"}
           justifyContent={"center"}
           gap={10}
-          sx={{
-          }}
+          sx={{}}
         >
           {charactersData.data.results.map((character: any) => (
             <HeroCard key={character.id} props={character} />
           ))}
-         
-          
-        </Box>)}
-        {status === "succeeded" &&
-        <Box sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: '20px 0px'
-        }}>
-        <Stack spacing={2}  zIndex={12}  >
-          <Pagination color="secondary" sx={{
-            backgroundColor: '#ffffff',
-            padding: '5px',
-            border: '1px solid gray',
-            color: '#ffffff',
-            borderRadius: '40px'
-          }}  count={Math.round(charactersData.data.total / 20)} page={page} onChange={handleChange} />
-        </Stack>
         </Box>
-        }
+      )}
+      {status === "succeeded" && (
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "20px 0px",
+          }}
+        >
+          <Stack spacing={2} zIndex={12}>
+            <Pagination
+              color="secondary"
+              sx={{
+                backgroundColor: "#ffffff",
+                padding: "5px",
+                border: "1px solid gray",
+                color: "#ffffff",
+                borderRadius: "40px",
+              }}
+              count={Math.round(charactersData.data.total / 20)}
+              page={page}
+              onChange={handleChange}
+            />
+          </Stack>
+        </Box>
+      )}
       )
     </>
   );
